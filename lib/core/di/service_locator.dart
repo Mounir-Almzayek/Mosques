2815c016ac5a18e5f5a5980c246e83c6a223fa65
Mounir <mouneeralmzayek@gmail.com>
2026-05-29 07:45:41 +0300

    feat(di): register cache store, image store, image sync; init on boot

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
+import '../cache/cache.dart';
 import '../../data/repositories/app_settings_repository.dart';
 import '../../data/repositories/interfaces/app_settings_repository_interface.dart';
 import '../../data/repositories/interfaces/auth_repository_interface.dart';
@@ -17,6 +18,13 @@ void setupServiceLocator() {
   // External dependencies
   sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   sl.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+
+  // Cache infrastructure
+  sl.registerLazySingleton<ICacheStore>(() => HiveCacheStore());
+  sl.registerLazySingleton<OfflineImageStore>(() => OfflineImageStore());
+  sl.registerLazySingleton<ImageSyncService>(
+    () => ImageSyncService(sl<OfflineImageStore>()),
+  );
 
   // Auth
   sl.registerLazySingleton<IAuthRepository>(
